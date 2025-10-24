a46f60f5eea506f689098069be109f1b67757c91
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-24 23:48:43 +0900

    feat: implement application logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import racingcar.game.GameWinners;
+import racingcar.game.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: refactor
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String carNamesSplitByComma = Console.readLine();
+        var carNames = carNamesSplitByComma.split("\\s*,\\s*");
+        List<String> names = Arrays.stream(carNames).filter(name -> !name.isEmpty()).toList();
+        var racingGame = new RacingGame(names);
+
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        String number = Console.readLine();
+        long iteration = Long.parseLong(number);
+
+        System.out.println("실행 결과");
+        while (iteration-- > 0) {
+            racingGame.iterateSingleLap();
+            System.out.println(racingGame);
+        }
+
+        GameWinners winners = racingGame.getWinners();
+        System.out.printf("최종 우승자 : %s%s", winners.toString(), System.lineSeparator());
     }
 }
