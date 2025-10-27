35245174edf041f03d7d217c873397ee9fcb750a
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-10-27 17:50:11 +0900

    refactor: wrapping external library

--- src/main/java/racingcar/util/external/MissionUtil.java
+++ src/main/java/racingcar/util/external/MissionUtil.java
@@ -2,7 +2,7 @@ package racingcar.util.external;
 
 public class MissionUtil {
 
-    public static MissionUtil instance = new MissionUtil();
+    public final static MissionUtil instance = new MissionUtil();
 
     public String readLine() {
         return camp.nextstep.edu.missionutils.Console.readLine();
